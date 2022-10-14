3d1236dfbb4b697f61f7b75762c4bfd599106053
V Mendez <victormendez990@gmail.com>
2022-10-14 15:40:00 -0600

    Start template function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Import required modules
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Import helper functions in src folder
+const generate = require('./src/generateHTML.js')
