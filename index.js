636e347f0a4b6844d0f676cdcc74b9423a707dec
V Mendez <victormendez990@gmail.com>
2022-10-15 14:01:09 -0600

    Init function

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const fs = require('fs');
 
 // Import helper functions in src folder
 const generate = require('./src/generateHTML.js')
+
+// Function for app initiation 
+function init() {
+
+}
+
+// Calling for the init function to start the program. 
+init()
