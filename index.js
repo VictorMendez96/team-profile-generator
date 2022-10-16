5d12866f194eec21c7d9ecee62d964864f98fd4c
V Mendez <victormendez990@gmail.com>
2022-10-15 22:34:10 -0600

    Start Menu Function

--- index.js
+++ index.js
@@ -7,10 +7,35 @@ const fs = require('fs');
 const generate = require('./src/generateHTML.js');
 
 // Initiate inquirer interrupted prompt for prompts
-interruptedPrompt.fromAll(inquirer);
+inquirer.registerPrompt('input', interruptedPrompt);
 
 // Function for app initiation 
 function init() {
+    menu();
+};
+
+// Function for menu
+function menu() {
+    inquirer
+        .prompt([
+            {
+                name: 'menu',
+                type: 'list',
+                message: '',
+                choices: ['Add a new Manager', 'Add a new Engineer', 'Add a new Intern', 'Done! Make my Page!']
+            }
+        ])
+        .then((menuOption) => {
+            if(menuOption.includes('')) {
+
+            } else if (menuOption.includes('')) {
+
+            }else if (menuOption.includes('')) {
+                
+            } else {
+                
+            }
+        })
 
 };
 
@@ -42,7 +67,15 @@ function newManager() {
                 message: '',
                 default: '',
             },
-        ]);
+        ])
+        .then((responses) => {
+
+        })
+        .catch((error) => {
+            if(error === interruptedPrompt.EVENT_INTERRUPTED) {
+                console.log('exited to Menu :)');
+            }
+        })
 };
 
 // Calling for the init function to start the program. 
