faf40a72993cd5786bd2774f77d430013e0cb248
V Mendez <victormendez990@gmail.com>
2022-10-15 23:22:43 -0600

    Start functions for New Engineer and Intern

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ function menu() {
             }
         ])
         .then((menuOption) => {
-            if(menuOption.includes('')) {
-
-            } else if (menuOption.includes('')) {
-
-            }else if (menuOption.includes('')) {
-                
+            if(menuOption.includes('Add a new Manager')) {
+                newManager()
+            } else if (menuOption.includes('Add a new Engineer')) {
+                newEngineer()
+            }else if (menuOption.includes('Add a new Intern')) {
+                newIntern()
             } else {
-                
+
             }
         })
 
 };
 
-// Function to add Manager at start of program
+// Function to add a new Manager to the team
 function newManager() {
     inquirer
         .prompt([
@@ -73,7 +73,85 @@ function newManager() {
         })
         .catch((error) => {
             if(error === interruptedPrompt.EVENT_INTERRUPTED) {
-                console.log('exited to Menu :)');
+                console.log('Exited to Menu :)');
+            }
+        })
+};
+
+// Function to add a new Engineer to the team
+function newEngineer() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+            {
+                name: 'officeNumber',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+        ])
+        .then((responses) => {
+            
+        })
+        .catch((error) => {
+            if(error === interruptedPrompt.EVENT_INTERRUPTED) {
+                console.log('Exited to Menu :)');
+            }
+        })
+};
+
+// Function to add a new Intern at start of program
+function newIntern() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+            {
+                name: 'officeNumber',
+                type: 'input',
+                message: '',
+                default: '',
+            },
+        ])
+        .then((responses) => {
+            
+        })
+        .catch((error) => {
+            if(error === interruptedPrompt.EVENT_INTERRUPTED) {
+                console.log('Exited to Menu :)');
             }
         })
 };
