57ae1d6ce250223980a2d8387dd3924233c7693c
V Mendez <victormendez990@gmail.com>
2022-10-15 21:08:38 -0600

    Install and import interrupted prompt

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // Import required modules
 const inquirer = require('inquirer');
+const interruptedPrompt = require('inquirer-interrupted-prompt');
 const fs = require('fs');
 
 // Import helper functions in src folder
-const generate = require('./src/generateHTML.js')
+const generate = require('./src/generateHTML.js');
+
+// Initiate inquirer interrupted prompt for prompts
+interruptedPrompt.fromAll(inquirer);
 
 // Function for app initiation 
 function init() {
 
-}
+};
+
+// Function to add Manager at start of program
+function newManager() {
+    inquirer
+        .prompt([
+            {
+                name: '',
+                type: 'input',
+                message: '',
+                default: ''
+                
+            };
+        ]);
+};
 
 // Calling for the init function to start the program. 
 init()
