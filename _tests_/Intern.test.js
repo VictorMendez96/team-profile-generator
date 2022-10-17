04313b6f646263216756c76a80c5e082167a7644
V Mendez <victormendez990@gmail.com>
2022-10-16 23:12:02 -0600

    Complete Tests

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,21 @@
 const Intern = require('../lib/Intern')
+
+describe('Intern', () => {
+    test('Intern extends Employee class', () => {
+        const school = 'Denver University'
+        const employee = new Intern('John', '4890', 'John@test.com', school)
+        expect(employee.school).toBe(school)
+    })
+
+    test('Can retrieve school from Intern', () => {
+        const school = 'Denver University'
+        const employee = new Intern('John', '4890', 'John@test.com', school)
+        expect(employee.getSchool()).toBe(school)
+    })
+
+    test('Can retrieve role from Intern', () => {
+        const role = 'Intern'
+        const employee = new Intern('John', '4890', 'John@test.com')
+        expect(employee.getRole()).toBe(role)
+    })
+})
