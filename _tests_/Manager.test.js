a10509b76060386a6016d8d89bd5aafec9ec6c1f
V Mendez <victormendez990@gmail.com>
2022-10-16 23:09:08 -0600

    Edit Manager test

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -3,19 +3,19 @@ const Manager = require('../lib/Manager')
 describe('Manager', () => {
     test('Manager extends Employee class', () => {
         const officeNumber = '1020'
-        const employee = new Engineer('John', '4890', 'John@test.com', officeNumber)
+        const employee = new Manager('John', '4890', 'John@test.com', officeNumber)
         expect(employee.officeNumber).toBe(officeNumber)
     })
 
     test('Can retrieve officeNumber from Manager', () => {
         const officeNumber = '1020'
-        const employee = new Engineer('John', '4890', 'John@test.com', officeNumber)
+        const employee = new Manager('John', '4890', 'John@test.com', officeNumber)
         expect(employee.getOfficeNumber()).toBe(officeNumber)
     })
 
     test('Can retrieve role from Manager', () => {
         const role = 'Manager'
-        const employee = new Engineer('John', '4890', 'John@test.com')
+        const employee = new Manager('John', '4890', 'John@test.com')
         expect(employee.getRole()).toBe(role)
     })
 })
