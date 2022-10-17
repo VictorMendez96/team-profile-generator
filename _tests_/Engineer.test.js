d03a0f5e927771915204eea756d2d7ce941d62ee
V Mendez <victormendez990@gmail.com>
2022-10-16 23:02:39 -0600

    Complete Engineer tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,21 @@
-const Engineer = require('../lib/Engineer')
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+    test('Engineer extend class', () => {
+        const github = 'testgithub'
+        const employee = new Engineer('John', '4890', 'John@test.com', github)
+        expect(employee.github).toBe(github)
+    })
+
+    test('Can retrieve github from Engineer', () => {
+        const github = 'testgithub'
+        const employee = new Engineer('John', '4890', 'John@test.com', github)
+        expect(employee.getGithub()).toBe(github)
+    })
+
+    test('Can retrieve role from Engineer', () => {
+        const role = 'Engineer'
+        const employee = new Engineer('John', '4890', 'John@test.com')
+        expect(employee.getRole()).toBe(role)
+    })
+})
