6875d10abc9561a41cb6d36f5a1c0de975cd71d3
V Mendez <victormendez990@gmail.com>
2022-10-16 22:52:49 -0600

    Finish Employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee');
 
-test('Employee', () => {
+describe('Employee', () => {
     test('Employee Creation', () => {
         const employee = new Employee();
         expect(typeof(employee)).toBe('object') 
@@ -8,37 +8,43 @@ test('Employee', () => {
 
     test('Can set name', () => {
         const name = 'John';
-        const employee = new Employee();
+        const employee = new Employee(name);
         expect(employee.name).toBe(name);
     })
 
-    test('Can set name', () => {
-        const name = 'John';
-        const employee = new Employee();
-        expect(employee.name).toBe(name);
+    test('Can set id', () => {
+        const id = '4890';
+        const employee = new Employee('John', id);
+        expect(employee.id).toBe(id);
     })
 
-    test('Can set name', () => {
-        const name = 'John';
-        const employee = new Employee();
-        expect(employee.name).toBe(name);
+    test('Can set email', () => {
+        const email = 'John@test.com';
+        const employee = new Employee('John', '4890', email);
+        expect(employee.email).toBe(email);
     })
 
-    test('Can set name', () => {
-        const name = 'John';
-        const employee = new Employee();
-        expect(employee.name).toBe(name);
+    test('Can retrieve role', () => {
+        const role = 'Employee';
+        const employee = new Employee('John', '4890', 'John@test.com');
+        expect(employee.getRole()).toBe(role);
     })
 
-    test('Can set name', () => {
+    test('Can retrieve name', () => {
         const name = 'John';
-        const employee = new Employee();
-        expect(employee.name).toBe(name);
+        const employee = new Employee(name);
+        expect(employee.getName()).toBe(name);
     })
 
-    test('Can set name', () => {
-        const name = 'John';
-        const employee = new Employee();
-        expect(employee.name).toBe(name);
+    test('Can retrieve id', () => {
+        const id = '4890';
+        const employee = new Employee('John', id);
+        expect(employee.getId()).toBe(id);
+    })
+
+    test('Can retrieve email', () => {
+        const email = 'John@test.com';
+        const employee = new Employee('John', '4890', email);
+        expect(employee.getEmail()).toBe(email);
     })
 });
